250902280dc2d636616d4c1cd8bedce43ce9a3a2
nelsondavenapalli <nelson.csits@gmail.com>
2018-06-05 23:46:12 +0200

    Using DFS to check whether a upstream dependency is present and then add the operation.

--- src/main/java/com/fun/pipeline/pipeline/PipelineGenerator.java
+++ src/main/java/com/fun/pipeline/pipeline/PipelineGenerator.java
@@ -3,24 +3,77 @@ package com.fun.pipeline.pipeline;
 import com.fun.pipeline.domain.Operation;
 import com.fun.pipeline.domain.UpstreamAdjacencyMatrix;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PipelineGenerator {
 
     private UpstreamAdjacencyMatrix upstreamAdjacencyMatrix;
 
+    private boolean[] visitedFlag;
+
     private Set<Operation> operations;
 
 
     public PipelineGenerator(Set<Operation> operations) {
         this.operations = operations;
+        visitedFlag = new boolean[operations.size() + 1];
     }
 
+
+    public UpstreamAdjacencyMatrix updateUpstreamAdjacencyMatrix() {
+
+        upstreamAdjacencyMatrix = new UpstreamAdjacencyMatrix(operations.size() + 1, operations.size() + 1);
+
+        operations.forEach(operation -> operation.getUpstreamDep().forEach(upstreamOperation -> {
+            upstreamAdjacencyMatrix.addEdge(upstreamOperation.getId(), operation.getId());
+        }));
+
+        return upstreamAdjacencyMatrix;
+    }
 
     public List<Operation> getPipelineOfSortedUpstreamDependencies() {
 
-        return operations.stream().collect(Collectors.toList());
+        updateUpstreamAdjacencyMatrix();
+
+        System.out.println("upstreamAdjacencyMatrix = " + upstreamAdjacencyMatrix);
+
+        Stack<Operation> operationsStack = new Stack<>();
+
+        Stack<Operation> sortedOperationsBasedOnUpstreamDeps = new Stack<>();
+
+        this.operations.forEach(operationToBeIterated -> {
+
+            operationsStack.push(operationToBeIterated);
+
+            pushOperationOnlyIfNotPresent(sortedOperationsBasedOnUpstreamDeps, operationToBeIterated);
+
+            Iterator<Integer> iterator = upstreamAdjacencyMatrix.adj(operationsStack.pop().getId()).iterator();
+
+            if (!visitedFlag[operationToBeIterated.getId()]) {
+                while (iterator.hasNext()) {
+                    Integer operationId = iterator.next();
+                    Operation operationWhoseUpstreamDepNeedsToBeCalculated = this.operations.stream()
+                            .filter(operation -> operation.getId() == operationId).collect(Collectors.toList()).get(0);
+                    operationsStack.push(operationWhoseUpstreamDepNeedsToBeCalculated);
+
+                    pushOperationOnlyIfNotPresent(sortedOperationsBasedOnUpstreamDeps, operationWhoseUpstreamDepNeedsToBeCalculated);
+                }
+                visitedFlag[operationToBeIterated.getId()] = true;
+            }
+
+        });
+        List<Operation> collect = new ArrayList<>(sortedOperationsBasedOnUpstreamDeps);
+        Collections.reverse(collect);
+
+        return collect;
     }
+
+    private void pushOperationOnlyIfNotPresent(Stack<Operation> sortedOperationsBasedOnUpstreamDeps, Operation operationToBeIterated) {
+        if (sortedOperationsBasedOnUpstreamDeps.stream()
+                .filter(operation -> operation.getId() == operationToBeIterated.getId())
+                .collect(Collectors.toList()).size() == 0)
+            sortedOperationsBasedOnUpstreamDeps.push(operationToBeIterated);
+    }
+
 }
--- src/test/java/com/fun/pipeline/pipeline/PipelineGeneratorTest.java
+++ src/test/java/com/fun/pipeline/pipeline/PipelineGeneratorTest.java
@@ -1,9 +1,11 @@
 package com.fun.pipeline.pipeline;
 
 import com.fun.pipeline.domain.Operation;
+import com.fun.pipeline.domain.UpstreamAdjacencyMatrix;
 import org.assertj.core.util.Sets;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -47,6 +49,55 @@ public class PipelineGeneratorTest {
 
         assertThat(pipelineOfSortedUpstreamDependencies).hasSize(2);
         assertThat(pipelineOfSortedUpstreamDependencies).contains(operation1, operation2);
+
+    }
+
+    @Test
+    public void shouldGenerateAPipelineForFourOperationsWithOneUpstreamDepAndNoDownstreamDep() throws Exception {
+
+        Set<Operation> operations = Sets.newHashSet();
+
+        Operation operation1 = new Operation(1, emptyList(), emptyList());
+        Operation operation2 = new Operation(2, singletonList(operation1), emptyList());
+        Operation operation3 = new Operation(3, singletonList(operation2), emptyList());
+        Operation operation4 = new Operation(4, singletonList(operation3), emptyList());
+
+        operations.add(operation2);
+        operations.add(operation1);
+        operations.add(operation4);
+
+        operations.add(operation3);
+
+        PipelineGenerator pipelineGenerator = new PipelineGenerator(operations);
+
+        List<Operation> pipelineOfSortedUpstreamDependencies = pipelineGenerator.getPipelineOfSortedUpstreamDependencies();
+
+        assertThat(pipelineOfSortedUpstreamDependencies).hasSize(4);
+        assertThat(pipelineOfSortedUpstreamDependencies).containsSequence(operation4, operation3, operation2, operation1);
+
+    }
+
+    @Test
+    public void shouldCreateAUpstreamDepMatrix() throws Exception {
+
+        Set<Operation> operations = Sets.newHashSet();
+
+        Operation operation1 = new Operation(1, emptyList(), emptyList());
+        Operation operation2 = new Operation(2, Arrays.asList(operation1), emptyList());
+        Operation operation3 = new Operation(3, Arrays.asList(operation2, operation1), emptyList());
+        operations.add(operation2);
+        operations.add(operation1);
+        operations.add(operation3);
+
+        PipelineGenerator pipelineGenerator = new PipelineGenerator(operations);
+
+        UpstreamAdjacencyMatrix upstreamAdjacencyMatrix = pipelineGenerator.updateUpstreamAdjacencyMatrix();
+
+        //Upstream dependency -
+        assertThat(upstreamAdjacencyMatrix.contains(2, 1));
+        assertThat(upstreamAdjacencyMatrix.contains(3, 2));
+        assertThat(upstreamAdjacencyMatrix.contains(3, 1));
+
 
     }
 }
